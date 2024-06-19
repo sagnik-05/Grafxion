da07512aeefd92813bb98c4fbbfc64537d4db8d6
sagnik-05 <98590203+sagnik-05@users.noreply.github.com>
2024-06-19 11:46:07 +0530

    setup completed

---
+++ grafxion/app/(root)/transformation/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TransformationPage = () => {
+  return (
+    <div>
+      TransformationPage
+      
+    </div>
+  )
+}
+
+export default TransformationPage
+// localhost/TransformationPage/id
+// each transformation is new and different so they'll have different id
