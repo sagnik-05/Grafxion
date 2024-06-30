3b398a137b8c1545d210bf4172045c59f9c365fd
sagnik-05 <98590203+sagnik-05@users.noreply.github.com>
2024-07-01 01:27:12 +0530

    sign-in fixed

--- grafxion/app/(root)/page.tsx
+++ grafxion/app/(root)/page.tsx
@@ -1,12 +1,26 @@
-import { UserButton } from "@clerk/nextjs";
-import React from "react";
+"use client"
+import { UserButton, SignIn, useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
 const Home = () => {
-  return (<div>
+  const { isLoaded, isSignedIn } = useUser();
+  const router = useRouter();
 
-    <p>Home</p>
-    <UserButton afterSignOutUrl="/"/>
-    </div>)
+  if (!isLoaded) {
+    return null; // Or you can return a loading spinner
+  }
+
+  if (!isSignedIn) {
+    router.push("/sign-in");
+    return null;
+  }
+
+  return (
+    <div>
+      <p>Home</p>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  );
 };
 
 export default Home;
